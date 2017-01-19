2dcb16ac3d7d5b092638f642921419eac6a878f8
iamthes <s91630277@gmail.com>
2017-01-19 22:19:46 +0400

    nodemon

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import * as Koa from 'koa';
 import { router } from './ping/index';
 
-new Koa()
+const server = new Koa()
 	.use(router.routes())
 	.listen(3000, () => {
 		console.log('Server started - http://localhost:3000');
