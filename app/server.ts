d441c171591eb3976dd3ef58353c23230f532c4b
iamthes <s91630277@gmail.com>
2017-04-16 22:27:37 +0400

    Response time

--- app/server.ts
+++ app/server.ts
@@ -6,12 +6,19 @@ import { connection } from './connection';
 import { errorHandler } from './error-handler';
 const bodyParser = require('koa-bodyparser');
 
-const server = new Koa()
-	.use(errorHandler())
-	.use(bodyParser())
-	.use(connection())
-	.use(pingRoutes)
-	.use(userRoutes)
-	.listen(3000, () => {
-		console.log('Server started - http://localhost:3000');
-	});
+const app = new Koa();
+if (process.env.NODE_ENV !== 'production') {
+    const koaResponseTime = require('koa-response-time');
+    app.use(koaResponseTime());
+}
+app.use(errorHandler());
+app.use(bodyParser());
+app.use(connection());
+app.use(pingRoutes);
+app.use(userRoutes);
+
+if (module.parent === null) {
+    const server = app.listen(3000, () => {
+        console.log('Server started - http://localhost:3000');
+    });
+}
--- app/user/index.ts
+++ app/user/index.ts
@@ -10,7 +10,14 @@ export async function createUser(k: Router.IRouterContext, next: any) {
 	k.body = await entityManager.persist(user);
 }
 
+export async function browseUser(k: Router.IRouterContext, next: any) {
+    let entityManager: EntityManager = k['entityManager'];
+    let [result, count] = await entityManager.findAndCount(User);
+    k.body = { result, count };
+}
+
 export const routes = new Router()
 	.prefix('/user')
-	.post('/', createUser)
+    .post('/', createUser)
+	.get('/browse', browseUser)
 	.routes();
